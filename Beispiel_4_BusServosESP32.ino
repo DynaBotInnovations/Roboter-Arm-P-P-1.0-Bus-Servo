32ea371698cae12f571d98579eccb55ebe3dda83
DynaBot-Innovations <dynabot-innovations@outlook.de>
2025-01-12 17:12:59 +0100

    Update Beispiel_4_BusServosESP32.ino

--- Beispiel_4_BusServosESP32.ino
+++ Beispiel_4_BusServosESP32.ino
@@ -1,7 +1,7 @@
 /*
 Dies ist ein Beispiel Programm für den Arduino Uno  
 Es kann beliebig erweitert oder verändert werden um es an die gewünschte Anwendung anzupassen.
-Achtung:      
+Achtung: Sie müssen zuerst die IDs von den Servos ändern      
 */
 
 #include <SCServo.h>  //die erforderliche Bibliothek    
@@ -26,31 +26,39 @@ void loop()
 {
   if(digitalRead(BUTTON_PIN) == LOW) //
   {
+    Serial.println("Bewege Servo 1");
     st.WritePosEx(1, 4095, 1000, 50);//servo(ID1,Greifer drehen) speed=1000，acc=50，move to position=4095.
-    delay(2000);
+    delay(5000);
     
-    st.WritePosEx(1, 2000, 1000, 50);//servo(ID1,Greifer drehen) speed=1000，acc=50，move to position=2000.
-    delay(2000);
-
+    Serial.println("Bewege Servo 11");
+    st.WritePosEx(1, 3500, 1000, 50);//servo(ID1,Greifer drehen) speed=1000，acc=50，move to position=2000.
+    delay(5000);
+    
+    Serial.println("Bewege Servo 2");
     st.WritePosEx(2, 4095, 3400, 50);//servo(ID2,Gelenk1 drehen) speed=3400，acc=50，move to position=4095.
-    delay(2000);
+    delay(5000);
     
-    st.WritePosEx(2, 2000, 5000, 50);//servo(ID2,Gelenk1 drehen) speed=5000，acc=50，move to position=2000.
-    delay(2000);
+    Serial.println("Bewege Servo 22");
+    st.WritePosEx(2, 3500, 5000, 50);//servo(ID2,Gelenk1 drehen) speed=5000，acc=50，move to position=2000.
+    delay(5000);
 
+    Serial.println("Bewege Servo 3");
     st.WritePosEx(3, 4095, 3400, 50);//servo(ID3,Gelenk2 drehen) speed=3400，acc=50，move to position=4095.
-    delay(2000);
+    delay(5000);
     
-    st.WritePosEx(3, 2000, 5000, 50);//servo(ID3,Gelenk2 drehen) speed=5000，acc=50，move to position=2000.
-    delay(2000);
-
+    Serial.println("Bewege Servo 33");
+    st.WritePosEx(3, 3500, 5000, 50);//servo(ID3,Gelenk2 drehen) speed=5000，acc=50，move to position=2000.
+    delay(5000);
+    
+    Serial.println("Bewege Servo 4");
     st.WritePosEx(4, 4095, 3400, 50);//servo(ID4,Greifer öffnen/schließen) speed=3400，acc=50，move to position=4095.
-    delay(2000);                     //der greifer darf nur in einem gewissen bereich geöffnet/geschlossen werden
+    delay(5000);                     //der greifer darf nur in einem gewissen bereich geöffnet/geschlossen werden
     
-    st.WritePosEx(4, 2000, 5000, 50);//servo(ID4,Greifer öffnen/schließen) speed=5000，acc=50，move to position=2000.
-    delay(2000);                     //der greifer darf nur in einem gewissen bereich geöffnet/geschlossen werden
+    Serial.println("Bewege Servo 44");
+    st.WritePosEx(4, 3500, 5000, 50);//servo(ID4,Greifer öffnen/schließen) speed=5000，acc=50，move to position=2000.
+    delay(5000);                     //der greifer darf nur in einem gewissen bereich geöffnet/geschlossen werden
     
-    //das Programm kann beliebig erweitert werden 
+    //das Programm kann beliebig erweitert/verändert werden 
 
   }
 }
